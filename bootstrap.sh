7b2420470809857cb6580f1a7420ea29457983dc
Luke Barbuto <luke.barbuto@gmail.com>
2014-02-10 20:50:01 -0800

    Update install command

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ cp -r ~/.vim ~/.vim.old 2>/dev/null
 cp -f ~/.vimrc ~/.vim.old 2>/dev/null
 rm -fr ~/.vim 2>/dev/null
 rm -f ~/.vimrc 2>/dev/null
-git clone https://github.com/krisleech/vimfiles ~/.vim && cd ~/.vim && rake install && ln -s ~/.vim/vimrc ~/.vimrc
+git clone https://github.com/lexun/vimfiles ~/.vim && cd ~/.vim && rake install && ln -s ~/.vim/vimrc ~/.vimrc
